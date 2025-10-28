411d2923435ec8cb9fc8b5c4180b2b18fd254c3f
Alexandre-Monteiro <48133463+alexandre-ponthinho@users.noreply.github.com>
2025-10-28 05:23:19 -0300

    Fix table definitions in dbteste.sql
    o not null é inutil na chave primaria e ainda com autoincrement

--- dbteste.sql
+++ dbteste.sql
@@ -1,12 +1,12 @@
 create table usuario (
-iduser int not null auto_increment primary key,
+iduser int auto_increment primary key,
 nomeuser varchar (255) not null
 );
 create table produtos (
-idprod int not null auto_increment primary key,
+idprod int auto_increment primary key,
 nomeprod varchar (255) not null,
 quantprod int not null
 );
 create table venda (
-idvenda int not null auto_increment primary key
+idvenda int auto_increment primary key
 )
