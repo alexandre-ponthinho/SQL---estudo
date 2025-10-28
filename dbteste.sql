2f7223a2bca22fef10770fe288295a4436e3c978
Alexandre-Monteiro <48133463+alexandre-ponthinho@users.noreply.github.com>
2025-10-28 05:39:27 -0300

    Modify usuario, produtos, and venda table schemas
    Updated table definitions to ensure 'iduser', 'idprod', and 'idvenda' are not null.

--- dbteste.sql
+++ dbteste.sql
@@ -1,12 +1,12 @@
 create table usuario (
-iduser int auto_increment primary key,
+iduser int not null auto_increment primary key,
 nomeuser varchar (255) not null
 );
 create table produtos (
-idprod int auto_increment primary key,
+idprod int not null auto_increment primary key,
 nomeprod varchar (255) not null,
 quantprod int not null
 );
 create table venda (
-idvenda int auto_increment primary key
+idvenda int not null auto_increment primary key
 )
